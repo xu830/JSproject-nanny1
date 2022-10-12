5255e05552b60647cccf0fa3a1f714fc65eeafc1
Yuening Xu <xuyn@gwu.edu>
2022-10-12 01:09:51 -0400

    funtional works

--- nanny1/src/components/Modal.js
+++ nanny1/src/components/Modal.js
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style/Modal.css";
 
 const Modal = ({ closeModal }) => {
+  const [emailInput, setEmail] = useState("");
+  const [passwordInput, setPassword] = useState("");
+  const addCredential = () => {
+    const credential = {
+      email: emailInput,
+      password: passwordInput,
+    };
+    console.log(credential.email, credential.password);
+    setEmail("");
+    setPassword("");
+  };
   return (
     <div className="ModalBackground">
-      Modal
-      <div className="modalContainer">
-        <button onClick={() => closeModal(false)}> X </button>
+      <div className="ModalContainer">
+        <button id="closeBtn" onClick={() => closeModal(false)}>
+          X
+        </button>
         <div className="title">
-          <h1>Sign in to your account</h1>
+          <p>Sign in to your account</p>
         </div>
+
         <div className="body">
-          <p>email</p>
+          <form>
+            <label htmlFor="email" className="labels">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={emailInput}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <label htmlFor="pwd" className="labels">
+              Password
+            </label>
+            <input
+              type="text"
+              id="pwd"
+              name="pwd"
+              value={passwordInput}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </form>
         </div>
         <div className="footer">
-          <button>sign in</button>
+          <button onClick={addCredential}>sign in</button>
           <button>sign up</button>
           <button>forget password</button>
         </div>
